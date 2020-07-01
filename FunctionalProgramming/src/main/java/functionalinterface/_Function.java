007ac97f2a70784726a68f3b76865ebaa6330f7a
Saad Saeed <awesomesaad.saeed2@gmail.com>
2020-07-01 15:55:52 -0400

    implemented a bifunction

--- FunctionalProgramming/src/main/java/functionalinterface/_Function.java
+++ FunctionalProgramming/src/main/java/functionalinterface/_Function.java
@@ -1,18 +1,30 @@
 package functionalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
 	public static void main(String[] args) {
 		System.out.println(increment(1));
 		System.out.println(incrementor.apply(4));
-		System.out.println(incrementor.andThen(doubler).apply(4));	// composite functions, just like in math
+		System.out.println(incrementor.andThen(doubler).apply(4)); // composite functions, just like in math
+		System.out.println(incrementorAndMultiplier(2, 5));
+		System.out.println(incrementorAndMultiplierBiFunction.apply(2, 5));
 	}
-	
-	// imperative single-valued method 
+
+	// imperative single-valued method
 	public static int increment(int number) {
 		return number + 1;
 	}
-	static Function<Integer, Integer> incrementor = number -> number + 1;	// implementing the apply method
+
+	static Function<Integer, Integer> incrementor = number -> number + 1; // implementing the apply method
 	static Function<Integer, Integer> doubler = number -> number * 2;
+
+	// imperative 2-arg method
+	public static int incrementorAndMultiplier(int number, int factor) {
+		return (number + 1) * factor;
+	}
+	
+	// Bi-function: 2 inputs
+	static BiFunction<Integer, Integer, Integer> incrementorAndMultiplierBiFunction = (number, factor) -> (number + 1) * factor; 
 }
